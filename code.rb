2b8b93ca34b10486f2898873ef736e6e6a437d05
jcincotta22 <jcincotta22@gmail.com>
2016-08-15 18:25:58 -0400

    added files

---
+++ code.rb
@@ -0,0 +1,9 @@
+num = 0
+while num <= 100 do
+  if num % 2 != 0
+    puts num
+    num += 1
+  else
+    num += 1
+  end
+end
---
+++ lib/odd_numbers.rb
@@ -0,0 +1,10 @@
+# YOUR CODE HERE
+num = nil
+while num <= 100 do
+  if num % 2 != 0
+    puts num
+    num += 1
+    end
+  else
+    num += 1
+end
